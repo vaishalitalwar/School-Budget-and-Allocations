c4c84cae561cb08ebca38c716416bdd4f86b5fe6
RetroAkabane <57164699+RetroAkabane@users.noreply.github.com>
2023-01-12 17:29:31 -0500

    Modified the allocation category scraper

--- scripts/AllocationCategoriesScraper.py
+++ scripts/AllocationCategoriesScraper.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[140]:
 
 
 from bs4 import BeautifulSoup, NavigableString, Tag
@@ -20,10 +20,8 @@ import time
 import re
 
 
-# In[ ]:
+# In[249]:
 
-
-url = 'https://www.nycenet.edu/offices/finance_schools/budget/DSBPO/allocationmemo/fy21_22/am_fy22_erf1.htm'
 
 chrome_options = webdriver.ChromeOptions()
 #save any files in the current working directory
@@ -34,94 +32,194 @@ service = ChromeService(executable_path=ChromeDriverManager().install())
 
 driver = webdriver.Chrome(service = service, options=chrome_options)
 
-driver.get(url)
+
+# In[270]:
 
 
-
-# In[134]:
-
-
-url = 'https://www.nycenet.edu/offices/finance_schools/budget/DSBPO/allocationmemo/fy21_22/am_fy22_pg1.htm'
-url_prefix = 'https://www.nycenet.edu/offices/finance_schools/budget/DSBPO/allocationmemo/fy21_22/'
-urls = []
-categories = []
-#convert the webpage to soup
-page = requests.get(url)
-soup = BeautifulSoup(page.text, 'html.parser')
-#find the webpage body with event details
-table = soup.find('table')
-
-ran = False
-
-for row in table:
-    try:
-        for a in row.find_all('a', href=True):
-            if a.text == 'Arts Supplemental Funding through Fair Student Funding':
-                ran = True
-            if ran:
-                urls.append(url_prefix + a['href'])
-                categories.append(a.text)
-    except:
-        continue
-
-
-
-# In[135]:
-
-
-urls
-
-
-# In[136]:
-
-
-for i, category_title in enumerate(categories):
-    categories[i] = re.sub("\\r|\\t|\\n","",category_title)
+def getAllocationCategories(year, driver):
     
-categories
-
-
-# In[137]:
-
-
-chrome_options = webdriver.ChromeOptions()
-#save any files in the current working directory
-#prefs = {'download.default_directory' : os.getcwd()}
-#chrome_options.add_argument("--headless")
-#chrome_options.add_experimental_option('prefs', prefs)
-service = ChromeService(executable_path=ChromeDriverManager().install())
-
-driver = webdriver.Chrome(service = service, options=chrome_options)
-
-driver.get(url)
-
-
-# In[138]:
-
-
-categories_dict = {}
-
-for i, url in enumerate(urls):
+    #Navigate to the memorandums page for the given year
+    url = 'https://infohub.nyced.org/reports/financial/financial-data-and-reports/school-allocation-memorandums'
+    year = str(year)
     driver.get(url)
-    funding_titles = []
-    elements = driver.find_elements(By.CSS_SELECTOR, '[style]')
+    link2memorandums = ''
+    elements = driver.find_elements(By.CSS_SELECTOR, 'a[target="_blank"]')
     for element in elements:
-        if ('fontweightbold' in (re.sub(r'\W+', '',element.get_attribute("style")).lower())) and ('colorblue' in (re.sub(r'\W+', '',element.get_attribute("style")).lower())):
-            funding_titles.append(element.text)
-    if '\n' in ''.join(funding_titles):
-        temp = funding_titles.copy()
-        for title in funding_titles:
-            if '\n' in title:
-                temp.remove(title)
-                temp = temp + title.split('\n')
-                funding_titles = temp
-    categories_dict[i] = ({"Category": categories[i], "Galaxy Titles": funding_titles})
+        if year in element.text:
+            link2memorandums = element.get_attribute('href')
+            break
+    driver.get(link2memorandums)
+    
+    #Go to the 	School Allocation Memorandums sorted numerically
+    driver.find_element(By.XPATH,"//*[contains(text(), 'SAMs by Numbers')]").click()
+    
+    #Make a list of all the links to allocation memorandums and category titles
+    url_prefix = link2memorandums.split('am_')[0]
+    urls = []
+    categories = []
+    page = requests.get(driver.current_url)
+    soup = BeautifulSoup(page.text, 'html.parser')
+    table = soup.find('table')
+
+    ran = False
+
+    for row in table:
+        try:
+            #FSF has multiple entries but we only need the memorandum
+            for a in row.find_all('a', href=True):
+                if a.text == 'Fair Student Funding Memorandum':
+                    urls.append(url_prefix + a['href'])
+                    categories.append(a.text)
+                #After 'Allocation Summary by District' marks the start of memorandum links
+                if 'Allocation Summary by District' in a.text:
+                    ran = True
+                    continue
+                #If you have passed 'Allocation Summary by District' start to save the category titles and links
+                if ran:
+                    if a['href'].startswith('http'):
+                        urls.append(a['href'])
+                    else:
+                        urls.append(url_prefix + a['href'])
+                    categories.append(a.text)
+        except:
+            continue
+    
+    #Formatting the titles
+    for i, category_title in enumerate(categories):
+        categories[i] = re.sub("\\r|\\t|\\n","",category_title)
+    
+    #List of dictionaries with allocation titles and galaxy listing
+    categories_list = []
+
+    #Add the galaxy listings for every allocation title
+    for i, url in enumerate(urls):
+        driver.get(url)
+        funding_titles = []
+        
+        #Find elements with style attritbute and are bold and blue
+        elements = driver.find_elements(By.CSS_SELECTOR, '[style]')
+        for element in elements:
+            if ('fontweightbold' in (re.sub(r'\W+', '',element.get_attribute("style")).lower())) and ('colorblue' in (re.sub(r'\W+', '',element.get_attribute("style")).lower()) or 'color0000FF' in (re.sub(r'\W+', '',element.get_attribute("style")).lower())):
+                funding_titles.append(element.text)
+                
+        #Find elements with bold tag that are blue
+        elements = driver.find_elements(By.TAG_NAME, 'b')
+        for parentElement in elements:
+            if (parentElement.find_elements(By.CSS_SELECTOR, '[color="#0000FF"]')):
+                childElements = (parentElement.find_elements(By.CSS_SELECTOR, '[color="#0000FF"]'))
+                for childElement in childElements:
+                    funding_titles.append(childElement.text)
+            elif (parentElement.find_elements(By.CSS_SELECTOR, '[style="color: blue"]')):
+                childElements = (parentElement.find_elements(By.CSS_SELECTOR, '[style="color: blue"]'))
+                for childElement in childElements:
+                    funding_titles.append(childElement.text)
+        
+        #Find elements that are blue (first) and have a child element with the bold tag
+        elements = driver.find_elements(By.CSS_SELECTOR, '[color="#0000FF"]')
+        for parentElement in elements:
+            if (parentElement.find_elements(By.TAG_NAME, 'b')):
+                childElements = (parentElement.find_elements(By.TAG_NAME, 'b'))
+                for childElement in childElements:
+                    funding_titles.append(childElement.text)
+        
+        #Find elements in a table that are blue and bold
+        elements = driver.find_elements(By.CSS_SELECTOR, 'tbody')
+        for element in elements:
+            if ('fontweightbold' in (re.sub(r'\W+', '',element.get_attribute("style")).lower())) and ('colorblue' in (re.sub(r'\W+', '',element.get_attribute("style")).lower()) or 'color0000FF' in (re.sub(r'\W+', '',element.get_attribute("style")).lower())):
+                funding_titles.append(element.text)
+        
+        #Formatting for galaxy titles
+        if '\n' in ''.join(funding_titles):
+            temp = funding_titles.copy()
+            for title in funding_titles:
+                if '\n' in title:
+                    temp.remove(title)
+                    temp = temp + title.split('\n')
+                    funding_titles = temp
+        
+        #Remove duplicates
+        funding_titles = list(set(funding_titles))
+        
+        
+        categories_list.append({"Category": categories[i], "Galaxy Titles": funding_titles})
+
+            
+    return categories_list
+    
 
 
-# In[139]:
+# In[271]:
 
 
-categories_dict
+allocationCategories2021 = getAllocationCategories(2021, driver)
+allocationCategories2021
+
+
+# In[268]:
+
+
+allocationCategories2020 = getAllocationCategories(2020, driver)
+allocationCategories2020
+
+
+# In[257]:
+
+
+allocationCategories2022 = getAllocationCategories(2022, driver)
+allocationCategories2022
+
+
+# In[208]:
+
+
+district_5_allocations = pd.read_csv(r'C:\Users\ebroh\BetaNYC\School Budgets\School-Budget-and-Allocations\data\district 5\allocation_district_5.csv')
+district_5_allocations
+
+
+# In[209]:
+
+
+M125_allocations = district_5_allocations[district_5_allocations['location_code'] == 'M125']
+M125_allocations
+
+
+# In[274]:
+
+
+M125_2022 = M125_allocations[M125_allocations['fiscal_year'] == 2022]
+M125_2022 = M125_2022.drop(columns=('Unnamed: 0'))
+M125_allocation_categories = M125_2022['allocation_category'].unique()
+all_2022_categories = []
+for i in allocationCategories2022:
+    all_2022_categories = all_2022_categories + i['Galaxy Titles']
+overlap = [x for x in M125_allocation_categories if x not in all_2022_categories]
+overlap
+
+
+# In[278]:
+
+
+M125_2021 = M125_allocations[M125_allocations['fiscal_year'] == 2021]
+M125_2021 = M125_2021.drop(columns=('Unnamed: 0'))
+M125_allocation_categories = M125_2021['allocation_category'].unique()
+all_2021_categories = []
+for i in allocationCategories2021:
+    all_2021_categories = all_2021_categories + i['Galaxy Titles']
+overlap = [x for x in M125_allocation_categories if x not in all_2021_categories]   
+overlap
+
+
+# In[279]:
+
+
+M125_2020 = M125_allocations[M125_allocations['fiscal_year'] == 2020]
+M125_2020 = M125_2020.drop(columns=('Unnamed: 0'))
+M125_allocation_categories = M125_2020['allocation_category'].unique()
+all_2020_categories = []
+for i in allocationCategories2020:
+    all_2021_categories = all_2021_categories + i['Galaxy Titles']
+overlap = [x for x in M125_allocation_categories if x not in all_2021_categories]   
+overlap
 
 
 # In[ ]:
